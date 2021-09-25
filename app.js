1f143e800f3f6ab5e0e18e0586b73fc2c49a8276
Lizzie Chai <lizzie.chai@gmail.com>
2021-09-24 20:00:10 -0500

    hw2

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const mongoose = require("mongoose");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -11,6 +13,19 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 
+const Dishes = require("./models/dishes");
+
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+
+connect.then(
+  (db) => {
+    console.log("Connected correctly to server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 var app = express();
 
 // view engine setup
