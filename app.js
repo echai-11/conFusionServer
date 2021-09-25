092c4479f7b9c1f2c52a1c29fdecf1d6af0aa90e
Lizzie Chai <lizzie.chai@gmail.com>
2021-09-25 13:46:31 -0500

    basic auth

--- app.js
+++ app.js
@@ -36,6 +36,39 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log("Request Headers", req.headers);
+
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error("You are not authenticated");
+
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+
+  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if (username == "admin" && password == "password") {
+    next(); // authorized
+  } else {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
+
+//auth before access to fetch resources
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
